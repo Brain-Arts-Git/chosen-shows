1e3dac070613c0c2f1e90b82131d55374eff000c
Jon <jonathanrodiger@gmail.com>
2019-10-22 09:04:23 -0400

    switched to selenium implementation

--- chosen_shows_scraper.py
+++ chosen_shows_scraper.py
@@ -1,69 +1,70 @@
-from bs4 import BeautifulSoup
-import requests
+#!/usr/bin/env python3
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-##########################
-## Chosen Shows Scraper ##
-## -------------------- ##
-## Jon Rodiger  -  2019 ##
-##########################
+# Chosen Shows Scraper
+# Jonathan Rodiger - 2019
+
+driver = webdriver.Chrome()
 
 with open('showlist.csv', 'w') as out:
 	out.write('EVENT NAME\tVENUE NAME\tORGANIZER NAME\tSTART DATE\tSTART TIME\tEND DATE\tEND TIME\tALL DAY EVENT\tCATEGORIES\tEVENT COST\tEVENT WEBSITE\tSHOW MAP LINK?\tSHOW MAP?\tEVENT DESCRIPTION\tDetails\tAttendance\n')
 	with open('november.txt', 'r') as f:
 		for line in f:
-			url = line.strip()
+			url = line.strip('\n')
+			driver.get(url)
 
-			# load html w/ requests
-			response = requests.get(url, headers={"content-type":"text"})
+			print(url)
 
-			# parse page content w/ beatiful soup
-			soup = BeautifulSoup(response.content, 'html.parser')
-			pretty_html = str(soup.prettify())
-
+			# event name
 			try:
-				# event name
-				event_name = soup.select('#seo_h1_tag')[0].get_text()
+				event_name = driver.find_element_by_id("seo_h1_tag").get_attribute("textContent")
+				print('event name:')
+				print(event_name)
 			except:
 				event_name = ''
 
+			# time + date
 			try:
-				# venue name
-				venue_name = pretty_html.split('"Place","name":"')[1].split('"')[0]
+				time_date = driver.find_element_by_class_name('_5xhk').get_attribute("textContent")
+				print('time and date:')
+				print(time_date)
+				# TODO: check if at shows up twice (event over multiple days)
+				# TODO: strip time zone info from end time
+				start_date = time_date.split(' at ')[0]
+				end_date = start_date
+				start_time, end_time = time_date.split(' at ')[1].split(' – ')
 			except:
-				venue_name = ''
+				pass
 
+			# venue name
 			try:
-				# address
-				address = pretty_html.split('</a><div class="_5xhp fsm fwn fcg">')[1].split('</div>')[0]
+				venue = driver.find_element_by_xpath("//a[@class='_5xhk']").get_attribute("textContent")
+			except:
+				try:
+					venue = driver.find_element_by_xpath("//span[@class='_5xhk'][1]").get_attribute("textContent")
+				except:
+					venue = ''
+			print('venue:')
+			print(venue)
+
+			# address
+			try:
+				address = driver.find_element_by_xpath("//*[@id='u_0_k']/table/tbody/tr/td[2]/div/div[1]/div/div[2]/div/div").get_attribute("textContent")
+				print('address:')
+				print(address)
 			except:
 				address = ''
 
+			# description
 			try:
-				# event description
-				description = pretty_html.split('}},"description":"')[1].split('","image":"')[0]
-				description = description.replace('\\/', '/')
+				description = driver.find_element_by_class_name('_63ew').get_attribute("textContent")
+				# print('description:')
+				# print(description)
 			except:
 				description = ''
 
-			try:
-				# start date + time
-				start_date_time = pretty_html.split(',"startDate":"')[1].split('"')[0]
-				start_date, start_time = start_date_time.split('T')
-				start_time = start_time.split('-')[0]
-			except:
-				start_date = ''
-				start_time = ''
-
-			try:
-				# end date + time
-				end_date_time = pretty_html.split('"endDate":"')[1].split('"')[0]
-				end_date, end_time = end_date_time.split('T')
-				end_time = end_time.split('-')[0]
-			except:
-				end_date = ''
-				end_time = ''
+			print('\n')
 
 			# write event info
-			out.write('\t'.join([event_name,venue_name,'',start_date,start_time,end_date,end_time,'','','',url,'','','',description,'']) + '\n')
-
-print('Success!')
+			out.write('\t'.join([event_name,venue,'',start_date,start_time,end_date,end_time,'','','',url,address,'','',description,'']) + '\n')
